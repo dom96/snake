f101cc621c19e0de024b09ffd3cca16259187341
Dominik Picheta <dominikpicheta@gmail.com>
2017-03-18 18:01:29 +0100

    Cleaned up onKeyDown procedure.

--- snake.nim
+++ snake.nim
@@ -8,22 +8,18 @@ proc onKeydown(game: Game, ev: Event) =
   let key = ev.keyCode.fromKeyCode()
   console.log("Pressed: ", $key)
 
-  var handled = false
+  var handled = true
   case key
   of Key.UpArrow:
     game.changeDirection(dirNorth)
-    handled = true
   of Key.RightArrow:
     game.changeDirection(dirEast)
-    handled = true
   of Key.DownArrow:
     game.changeDirection(dirSouth)
-    handled = true
   of Key.LeftArrow:
     game.changeDirection(dirWest)
-    handled = true
   else:
-    discard
+    handled = false
 
   if handled:
     ev.preventDefault()
