8015785952e0eef43c824e400905e18751146b5b
Dominik Picheta <dominikpicheta@gmail.com>
2017-03-26 20:55:55 +0200

    Make cheat detection a bit more lenient.

--- snake/server.nim
+++ snake/server.nim
@@ -105,8 +105,8 @@ proc processMessage(server: Server, client: Client, data: string) {.async.} =
       warn("Nickname too long, truncating")
       client.player.nickname = client.player.nickname[0 .. 8]
   of MessageType.ScoreUpdate:
-    let diff = msg.score - client.player.score
-    if diff < 0 or msg.score > server.top.score or not client.player.alive:
+    let diff = msg.score - server.top.score
+    if msg.score < 0 or diff > 5 or not client.player.alive:
       warn("Client ($1) is cheating" % $client)
       client.connected = false
       return
